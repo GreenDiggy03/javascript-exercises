1f20afb3be17f205bc0ed3b2edff9da8104151d5
GreenDiggy03 <greendiggy01@gmail.com>
2023-09-29 09:45:06 -0500

    Completed 6 to 8

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+function round(value, precision) {
+  // var multiplier = Math.pow(10, precision || 0);
+  // return Math.round(value * multiplier) / multiplier;
+  return Math.round(value * 10) / 10;
+}
+
+const convertToCelsius = function(degree) {
+  return round((degree - 32) * (5/9), 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  return round((degree * 1.8) + 32, 1);
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+	let sum = 0;
+  var numberArray = args.flat().map(Number);
+  
+  for (let i = 0; i < numberArray.length; i++) {
+    sum += numberArray[i];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(...args) {
+  let numberArray = args.flat().map(Number);
+  let product = numberArray[0];
 
+
+  for (let i = 1; i < numberArray.length; i++) {
+    product *= numberArray[i];
+  }
+  
+  return product;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+  return number ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 1 || num === 0)
+    return 1;
+
+  for (let i = num - 1; i >= 1; i--) {
+    num = num * i;
+  }
+
+  return num;
 };
 
 // Do not edit below this line
